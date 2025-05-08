8968dea5dd82666fd5e5fa421c5c4ac99b285f2c
goli <shirafkanzeynab63@gmail.com>
2025-05-08 18:14:05 +0330

    add CustomStack commands

--- src/datastructures/stacks/CustomStack.java
+++ src/datastructures/stacks/CustomStack.java
@@ -3,6 +3,8 @@ package datastructures.stacks;
 import datastructures.interfaces.Queue;
 import datastructures.lists.CustomLinkedList;
 
+import java.util.NoSuchElementException;
+
 public class CustomStack<T> implements Queue<T> {
     private CustomLinkedList<T> list;
 
@@ -10,6 +12,69 @@ public class CustomStack<T> implements Queue<T> {
         list = new CustomLinkedList<>();
     }
 
-    // TODO: Override and fill the methods to complete the data structure
+    @Override
+    public boolean add(Object t){
+        T object = (T) t;
+        list.addLast(object);
+        return true;
+    }
+
+    @Override
+    public boolean offer(Object o){
+        T t = (T) o;
+        if (add(t) == true){
+            return true;
+        }
+        else return false;
+    }
+
+    @Override
+    public T remove(){
+        if (list.isEmpty()){
+            throw new NoSuchElementException("The list is empty.");
+        }
+        T t = list.getFirst();
+        list.removeFirst();
+        return t;
+    }
+
+    @Override
+    public T poll(){
+        if (list.isEmpty()){
+            return null;
+        }
+        T t = list.getFirst();
+        list.removeFirst();
+        return t;
+    }
+
+    @Override
+    public T element(){
+        if (list.isEmpty()){
+            throw new NoSuchElementException("The list is empty.");
+        }
+        return list.getFirst();
+    }
+
+    @Override
+    public T peek(){
+        if (list.isEmpty()){
+            return null;
+        }
+        return list.getFirst();
+    }
+
+    @Override
+    public boolean isEmpty(){
+        if (list.size() == 0){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int size(){
+        return list.size();
+    }
 
 }
