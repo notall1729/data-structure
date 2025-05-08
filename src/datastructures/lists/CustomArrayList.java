7fc1ae3ca28aae8d416c92ec56cb805aa1da57c4
goli <shirafkanzeynab63@gmail.com>
2025-05-08 12:39:26 +0330

    add CustomArrayList commands

--- src/datastructures/lists/CustomArrayList.java
+++ src/datastructures/lists/CustomArrayList.java
@@ -19,6 +19,74 @@ public class CustomArrayList<T> implements List<T> {
         }
     }
 
-    // TODO: Override and fill the methods to complete the data structure
+    @Override
+    public T get(int index){
+        if (index < 0 || index >= size){
+            throw new IndexOutOfBoundsException("This index is not correct.");
+        }
+        return (T)elements[index];
+    }
+
+    @Override
+    public T set(int index, T element){
+        if (index < 0 || index >= size){
+            throw new IndexOutOfBoundsException("This index is not correct.");
+        }
+
+        elements[index] = element;
+        return element;
+    }
+
+    @Override
+    public boolean isEmpty(){
+        if (size == 0){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean add(Object o){
+        if (o == null){
+            throw new NullPointerException("This object is null.");
+        }
+        size ++;
+        ensureCapacity();
+        elements[size - 1] = o;
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o){
+        if (o == null){
+            throw new NullPointerException("This object is null.");
+        }
+
+        for (int i = 0; i < size; ++ i){
+            if (o.equals(elements[i])){
+                for (int j = i; j < size - 1; ++ j){
+                    elements[j] = elements[j + 1];
+                }
+                size --;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int size(){
+        return size;
+    }
+
+    @Override
+    public boolean contains(Object o){
+        for (Object O : elements){
+            if(o.equals(O)){
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
