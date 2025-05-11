58583c4d6c61defcc20dd7b7ffd4e7153e582a87
goli <shirafkanzeynab63@gmail.com>
2025-05-11 18:52:11 +0330

    update Book commands

--- src/library/Book.java
+++ src/library/Book.java
@@ -1,20 +1,20 @@
 package library;
 
+import datastructures.queues.CustomQueue;
+
 public class Book {
     private String title;
     private String author;
     private String isbn;
     private boolean isAvailable;
-
-    // TODO: Define a data structure to hold members waiting for this book
+    private CustomQueue<Member> waitlist;
 
     public Book(String title, String author, String isbn) {
         this.title = title;
         this.author = author;
         this.isbn = isbn;
         this.isAvailable = true;
-
-        // TODO: Initialize your data structure here
+        waitlist = new CustomQueue<>();
     }
 
     public String getTitle() { return title; }
@@ -24,17 +24,15 @@ public class Book {
     public void setAvailable(boolean available) { isAvailable = available; }
 
     public void addToWaitlist(Member member) {
-        // TODO
+        waitlist.add(member);
     }
 
     public Member getNextInWaitlist() {
-        // TODO
-        return null;
+        return waitlist.poll();
     }
 
     public boolean hasWaitlist() {
-        // TODO
-        return false;
+        return !waitlist.isEmpty();
     }
 
     @Override
